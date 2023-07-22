fe610bd67457233eb4159e2933c2bd78b8fe1584
league <league@imac-29.attlocal.net>
2023-07-22 11:26:41 -0700

    badger

---
+++ src/_05_for_loops/_2_badgers/Badger.java
@@ -0,0 +1,47 @@
+package _05_for_loops._2_badgers;
+
+public class Badger {
+
+	public static void main(String[] args) {
+
+
+
+		for(int repeat=0; repeat<2; repeat++) {
+
+			for(int badger=1; badger<=12; badger++) {
+
+				System.out.print("Badger");
+			if(badger<12) {
+				System.out.print(", ");
+				}
+			}
+			
+			
+			System.out.println();
+
+
+			for(int mushroom=1; mushroom<=2; mushroom++) {
+
+				System.out.print("Mushroom");
+					
+				if(mushroom<2) {
+				
+				System.out.print(", ");
+				
+				}
+			}
+		System.out.println();
+System.out.println();
+		}	
+		
+		
+
+
+
+		for(int snake=1; snake<2; snake++) {
+
+			System.out.println("A SNAKE!!!!!");				
+		}
+
+	}
+}
